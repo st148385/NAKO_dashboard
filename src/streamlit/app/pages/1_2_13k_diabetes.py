8fd9feda73bad245bb17890548970e9d1f9d42ea
A <st185327@stud.uni-stuttgart.de>
2024-04-16 14:16:11 +0200

    small changes

--- src/streamlit/app/pages/1_2_13k_diabetes.py
+++ src/streamlit/app/pages/1_2_13k_diabetes.py
@@ -8,16 +8,20 @@ from utils.utils import create_distribution_plot, extract_infos_given_datapath
 # Hardcoding the data paths for now, might change in future
 # data_path = st.file_uploader("Select folder containing data information")
 
-root = Path("/home/artur/Desktop/FA/data/NAKO_tabular/13k_diabetes")
-metadata_path = root / "NAKO_536_61223_export_baseln_MRT_PSN_wie_194_Metadaten.csv"
-data_path = root / "NAKO_536_61223_export_baseln_MRT_PSN_wie_194.csv"
-data_description_path = root / "NAKO-536 - 2023-12-06 - Beschreibung des Übergabedatensatzes.html"
+# root = Path("/home/artur/Desktop/FA/data/NAKO_tabular/30k_metadata")
+root = st.text_input(
+	"Provide the root dir of 30 k metadata",
+	"/home/artur/Desktop/FA/data/NAKO_tabular/13k_diabetes",
+	key="root_path",
+	placeholder="/home/data/NAKO_tabular...",
+)
+if root:
+	root = Path(root)
 
-st.header("13 K diabetes dataset", divider=True)
+	metadata_path = root / "NAKO_536_61223_export_baseln_MRT_PSN_wie_194_Metadaten.csv"
+	data_path = root / "NAKO_536_61223_export_baseln_MRT_PSN_wie_194.csv"
+	data_description_path = root / "NAKO-536 - 2023-12-06 - Beschreibung des Übergabedatensatzes.html"
 
-
-if data_path:
-	st.markdown("Overview of the data")
 	data = pd.read_csv(data_path, sep=";", encoding="latin1", quoting=csv.QUOTE_NONE)
 	features = data.columns[1:]
 	col1, col2 = st.columns(2)
@@ -37,9 +41,9 @@ if data_path:
 	# Get values and corresponding counts from attr_info
 	data_distribution = attr_info[option].get("feature_stats").get("data_distribution", False)
 	if data_distribution:
+		st.pyplot(create_distribution_plot(data_distribution, title=option))
 		st.warning("""
 			The X-values have been squashed together to show in one plot and do not necessarily show the true spaces.
 			This results from the labeling convention
 			(e.g. some values are 1 and some are 9999).
 			""")
-		st.pyplot(create_distribution_plot(data_distribution, title=option))
